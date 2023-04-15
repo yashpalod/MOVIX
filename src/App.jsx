9057167caf266ea2bc7dea53d2965ff3ec1ea451
yashpalod <yashpalod01@gmail.com>
2023-04-15 23:23:51 +0530

    checking redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useState, useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
-
+  const dispatch = useDispatch()
+  const { url } = useSelector((state) =>
+    state.home
+  )
+  console.log(url)
   useEffect(() => {
     apiTesting();
   }, []);
@@ -10,9 +16,12 @@ function App() {
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
       console.log(res);
+      dispatch(getApiConfiguration(res));
     });
   };
-  return <div className="App">App</div>;
+  return <div className="App">App
+    {url?.total_pages}
+  </div>;
 }
 
 export default App;
