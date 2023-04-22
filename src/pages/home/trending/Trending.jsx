ea033120f00672027519f1c12b83be3833a3d75b
yashpalod <yashpalod01@gmail.com>
2023-04-22 10:43:56 +0530

    Calling Api on Clicking Tabs

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ContentWrapper from '../../../components/contentWrapper/ContenWrapper'
 import SwitchTabs from '../../../components/switchTabs/SwitchTabs'
+import useFetch from '../../../hooks/useFetch'
 
 const Trending = () => {
+    const [endPoint, setEndPoint] = useState("day")
+
+    const { data, loading } = useFetch(`/trending/all/${endPoint}`)
 
     const onTabChange = (tab) => {
-
+        setEndPoint(tab === "Day" ? "day" : "week");
     }
 
     return (
